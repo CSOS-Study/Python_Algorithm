2c3b647a8b4dec8569e64f7a1773b8562a4755e7
Junsu <loveskangi@gmail.com>
2021-04-15 19:40:46 +0900

    directory

--- test/10869.py
+++
@@ -1,3 +0,0 @@
-A,B,C = map(int,input().split())
-
-print((A+B)%C, ((A%C)+(B%C))%C, (A*B)%C, ((A%C)*(B%C))%C, sep='\n')
--- test/2588.py
+++
@@ -1,9 +0,0 @@
-A = int(input()) 
-B = input()      
-
-AB2 = A * int(B[2])
-AB1 = A * int(B[1])
-AB0 = A * int(B[0])
-AB = A * int(B)
-
-print(AB2, AB1, AB0, AB, sep='\n')
