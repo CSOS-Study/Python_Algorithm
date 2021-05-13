87360f6edb52dbd8fdf1038d05131e237b44a900
Junsu <loveskangi@gmail.com>
2021-05-13 23:33:25 +0900

    Munang

--- 백준 단계별로 풀어보기/12. 정렬/4. 통계학/강문영.py
+++ 백준 단계별로 풀어보기/12. 정렬/4. 통계학/강문영.py
@@ -5,6 +5,6 @@ n_list = sorted([int(sys.stdin.readline().strip()) for _ in range(n)])
 
 print(round(sum(n_list)/n))
 print(n_list[n//2])
-counts = collections.Counter(n_list).most_common()
-print(counts[0][0] if len(counts)==1 or counts[0][1]>counts[1][1] else counts[1][0])
+counts = collections.Counter(n_list).most_common(2)
+print(counts[-1][0] if counts[0][1] == counts[-1][1] else counts[0][0])
 print(n_list[-1]- n_list[0])
