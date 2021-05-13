2ba484077e5d95dbbdab92081fd2f66adaf06801
windsor <2615240@gmail.com>
2021-05-13 23:40:45 +0900

    [백준] 정렬

--- 백준 단계별로 풀어보기/12. 정렬/4. 통계학/이준수.py
+++ 백준 단계별로 풀어보기/12. 정렬/4. 통계학/이준수.py
@@ -5,16 +5,15 @@ N = int(sys.stdin.readline())
 list = [int(sys.stdin.readline()) for _ in range(N)]
 list.sort()
 
-print(round(sum(list)/N))
-print(list[N//2])
+print(round(sum(list) / N))
+print(list[N // 2])
 
-print(collections.Counter(list))
-lmc = collections.Counter(list).most_common(2)
+lmc = collections.Counter(list).most_common(2)  # n = 1 이면 1개뽑음 n >= 2인 경우 2개뽑음
 frequency = lmc[-1][0] if lmc[0][1] == lmc[-1][1] else lmc[0][0]
 
 print(frequency)
-print(max(list) - min(list))
+print(list[N - 1] - list[0])
 
-#삼항연산자
-#파이썬 반올림 round(실수,n)
-#list의 인덱스 순번대로(처음나오는값) collections.Counter(list)은 반환한다.
+# 삼항연산자
+# 파이썬 반올림 round(실수,n)
+# list의 인덱스 순번대로(처음나오는값) collections.Counter(list)은 반환한다.
