d503dc4cb2cdd93fac8b3dd07709b6164eb61248
windsor <2615240@gmail.com>
2021-05-13 23:05:35 +0900

    [백준] 정렬

--- 백준 단계별로 풀어보기/12. 정렬/4. 통계학/이준수.py
+++ 백준 단계별로 풀어보기/12. 정렬/4. 통계학/이준수.py
@@ -8,6 +8,7 @@ list.sort()
 print(round(sum(list)/N))
 print(list[N//2])
 
+print(collections.Counter(list))
 lmc = collections.Counter(list).most_common(2)
 frequency = lmc[-1][0] if lmc[0][1] == lmc[-1][1] else lmc[0][0]
 
@@ -16,3 +17,4 @@ print(max(list) - min(list))
 
 #삼항연산자
 #파이썬 반올림 round(실수,n)
+#list의 인덱스 순번대로(처음나오는값) collections.Counter(list)은 반환한다.
