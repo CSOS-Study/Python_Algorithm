f10019c7ea7dd1b9446e9a1d78bf303b65158c79
windsor <2615240@gmail.com>
2021-05-18 22:37:49 +0900

    [CodePlus] 중급1

--- CodePlus/중급/브루트포스-순열/1. 부등호/이준수.py
+++ CodePlus/중급/브루트포스-순열/1. 부등호/이준수.py
@@ -1,21 +1,19 @@
 from itertools import permutations
 
+def solve(number):
+    for i in permutations(number, N+1):
+        for idx in range(1, N+1):
+            if sign[idx - 1] == '<' and i[idx - 1] > i[idx]:
+                break
+            elif sign[idx - 1] == '>' and i[idx - 1] < i[idx]:
+                break
+        else:
+            return print(''.join(i))
+
 N = int(input())
 sign = input().split()
-number = list(map(int, range(10)))
-res = []
+number = "0123456789"
 
-for i in permutations(number, N+1):
-    flag = False
-    for idx in range(1,N+1):
-        if sign[idx-1] == '<' and i[idx-1] > i[idx]:
-            break
-        if sign[idx-1] == '>' and i[idx-1] < i[idx]:
-            break
-    else:
-        flag = True
-    if flag:
-        res.append(i)
+solve(number[::-1])
+solve(number)
 
-print(''.join(map(str,res[-1])))
-print(''.join(map(str,res[0])))
