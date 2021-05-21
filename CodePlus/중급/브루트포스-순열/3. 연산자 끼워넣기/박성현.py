b1fc6cb5d1e705de82771f55e4a002950f2efc6e
Seonghn <qkrtjdgus268@naver.com>
2021-05-21 22:20:08 +0900

    seonghyun

--- CodePlus/중급/브루트포스-순열/3. 연산자 끼워넣기/박성현.py
+++ CodePlus/중급/브루트포스-순열/3. 연산자 끼워넣기/박성현.py
@@ -1,32 +1,36 @@
-from itertools import permutations
+import sys
 
-n=int(input())
-case=list(map(int, input().split()))
-op=['+', '-', 'x', '//']
+n = int(input())
+case = list(map(int, input().split()))
+op = list(map(int, input().split()))
 
-opcase=[]
-for i,j in zip(op, list(map(int, input().split()))):
-    opcase.extend([i]*j)
-
-def oper(l, r, m):
-    if m=='+':
-        return l+r
-    elif m=='-':
-        return l-r
-    elif m=='x':
-        return l*r
-    else:
-        return int(l/r)
+max_res = -sys.maxsize
+min_res = sys.maxsize
 
 
-def sol():
-    global res
-    for i in permutations(opcase, n-1):
-        tmp=case[0]
-        for j in range(len(i)):
-            tmp=oper(tmp,case[j+1],i[j])
-        res.append(tmp)
+def dfs(cnt, res):
+    global max_res, min_res
+    if cnt == n:
+        max_res = max(max_res, res)
+        min_res = min(min_res, res)
+        return
+    if op[0] > 0:
+        op[0] -= 1
+        dfs(cnt + 1, res + case[cnt])
+        op[0] += 1
+    if op[1] > 0:
+        op[1] -= 1
+        dfs(cnt + 1, res - case[cnt])
+        op[1] += 1
+    if op[2] > 0:
+        op[2] -= 1
+        dfs(cnt + 1, res * case[cnt])
+        op[2] += 1
+    if op[3] > 0:
+        op[3] -= 1
+        dfs(cnt + 1, int(res / case[cnt]))
+        op[3] += 1
 
-res=[]
-sol()
-print(max(res), min(res),sep='\n')
+
+dfs(1, case[0])
+print(max_res, min_res, sep='\n')
