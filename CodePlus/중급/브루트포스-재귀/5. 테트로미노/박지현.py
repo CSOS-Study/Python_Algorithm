4eed0c1ac0ef612b7992f507d7309b59e9d4abc8
unknown <leehee0910@gmail.com>
2021-05-26 23:04:22 +0900

    jihyeon

--- CodePlus/중급/브루트포스-재귀/5. 테트로미노/박지현.py
+++ CodePlus/중급/브루트포스-재귀/5. 테트로미노/박지현.py
@@ -1,42 +1,39 @@
 import sys
 
-d = [(-1, 0), (0, 1), (1, 0), (0, -1)] # 모든 좌표 이동 방향
-dd = [[(0, -1), (-1, 0), (0, 1)], [(0, -1), (1, 0), (0, 1)], [(-1, 0), (1, 0), (0, 1)], [(-1, 0), (1, 0), (0, -1)]]
-# dfs로 탐색할 수 없는 ㅗ,ㅜ,ㅏ,ㅓ 는 notdfs()에서 따로 계산한다.
+go = [(-1,0),(0,1),(1,0),(0,-1)] # 좌표 이동 전방향 표현
+go_nodfs = [[(0, -1), (-1, 0), (0, 1)], [(0, -1), (1, 0), (0, 1)], [(-1, 0), (1, 0), (0, 1)], [(-1, 0), (1, 0), (0, -1)]]
+# dfs로 탐색할 수 없는 ㅗ,ㅜ,ㅏ,ㅓ 는 notdfs()에서 따로 계산
 
-def notdfs(r, c):
-    global answer
-    for dir in dd:
-        temp = paper[r][c]
-        for idx in range(3):
-            nr = r + dir[idx][0]
-            nc = c + dir[idx][1]
-            if 0 <= nr < N and 0 <= nc < M:
-                temp += paper[nr][nc]
-            else:
-                break
-        answer = max(answer, temp)
-
-def dfs(r, c, total, visited):
-    global answer
+def dfs(y,x,total,visited):
+    global mx
     if len(visited) == 4:
-        answer = max(answer, total)
+        mx = max(mx,total)
         return
-    for idx in range(4):
-        nr = r + d[idx][0]
-        nc = c + d[idx][1]
-        if 0 <= nr < N and 0 <= nc < M:
-            if (nr, nc) not in visited:
-                visited.append((nr, nc))
-                dfs(nr, nc, total + paper[nr][nc], visited)
+    for i in range(4):
+        yy = y + go[i][0]
+        xx = x + go[i][1]
+        if 0<=yy<n and 0<=xx<m:
+            if (yy,xx) not in visited:
+                visited.append((yy,xx))
+                dfs(yy,xx,total+table[yy][xx],visited)
                 visited.pop()
 
-N, M = map(int, input().split())
-paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+def no_dfs(y,x):
+    global mx
+    for i in go_nodfs:
+        total = table[y][x]
+        for j in range(3):
+            yy = y + i[j][0]
+            xx = x + i[j][1]
+            if 0<=yy<n and 0<=xx<m: total += table[yy][xx]
+            else: break
+        mx = max(mx,total)
 
-answer = 0
-for r in range(N):
-    for c in range(M):
-        dfs(r, c, paper[r][c], [(r, c)])
-        notdfs(r, c)
-print(answer)
+n, m = map(int,input().split())
+table = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+mx = 0
+for y in range(n):
+    for x in range(m):
+        dfs(y,x,table[y][x],[(y,x)])
+        no_dfs(y,x)
+print(mx)
