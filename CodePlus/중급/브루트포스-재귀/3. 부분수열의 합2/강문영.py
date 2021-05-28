0d2db21993cd4fb3d69030ed2f49ee581c9d3baa
Junsu <loveskangi@gmail.com>
2021-05-28 22:08:38 +0900

    Munang

--- CodePlus/중급/브루트포스-재귀/3. 부분수열의 합2/강문영.py
+++ CodePlus/중급/브루트포스-재귀/3. 부분수열의 합2/강문영.py
@@ -3,13 +3,14 @@ from itertools import combinations
 n = int(input())
 n_list = list(map(int,input().split()))
 
-check_list = [0]*100000000
+check_list = [0]*(sum(n_list)+2)
+check_list[0] = 1
+
+
+check_list = [1] + [0]*[n+1]
 
 for i in range(1,n+1):
     for j in combinations(n_list,i):
         check_list[sum(j)]=1
 
-for idx,val in enumerate(check_list):
-    if val == 0 and idx>0:
-        print(idx)
-        break
+print(check_list.index(0))
