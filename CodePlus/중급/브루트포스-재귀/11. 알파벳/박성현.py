2be546a76d6e24a8ce030a7ed6d6930d5462cb51
Seonghn <qkrtjdgus268@naver.com>
2021-05-28 19:59:20 +0900

    seonghyun

--- CodePlus/중급/브루트포스-재귀/11. 알파벳/박성현.py
+++ CodePlus/중급/브루트포스-재귀/11. 알파벳/박성현.py
@@ -21,3 +21,21 @@ res = 0
 dfs(0, 0, case[0][0])
 print(res)
 
+# 준수 풀
+R, C = map(int, input().split())
+board = [list(input())for _ in range(R)]
+path = set()
+
+
+def dfs(y, x):
+    res = 1
+    path.add(board[y][x])
+    for dy, dx in (-1,0), (1,0), (0,-1), (0,1):
+        ny = y + dy
+        nx = x + dx
+        if 0 <= ny < R and 0 <= nx < C and board[ny][nx] not in path:
+            res = max(res, 1 + dfs(ny, nx))
+    path.remove(board[y][x])
+    return res
+
+print(dfs(0, 0))
