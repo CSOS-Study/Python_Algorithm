4e0326f1615137bafc7e3950a73f78ccd1660f1d
Munang <loveskangi@gmail.com>
2021-11-02 00:35:04 +0900

    Munang

--- Programmers/알고리즘/Level2/전화번호 목록/강문영.py
+++ Programmers/알고리즘/Level2/전화번호 목록/강문영.py
@@ -1,10 +1,19 @@
-import re
+# 모범 답안
+# import re
+#
+# def solution(phone_book):
+#     phone_book.sort()
+#     for i in range(len(phone_book)-1): #O(n)
+#         if phone_book[i]<phone_book[i+1]:
+#             if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+#                 return False
+#     else:
+#         return True
 
+# 11/1 멍청한 풀이.. 다시 회개하자  무냉공주
 def solution(phone_book):
-    phone_book.sort()
-    for i in range(len(phone_book)-1): #O(n)
-        if phone_book[i]<phone_book[i+1]:
-            if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+    for idx, num in enumerate(phone_book):
+        for cpr_num in phone_book[:idx]+phone_book[idx+1:]:
+            if num==cpr_num[:len(num)]:
                 return False
-    else:
-        return True
+    return True
